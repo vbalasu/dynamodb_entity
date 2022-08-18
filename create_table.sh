2d294728126d90c51926dc53c4288935b1dba8d3
Vijay Balasubramaniam <vbalasu@gmail.com>
2022-08-17 19:24:50 -0700

    Initial commit

---
+++ create_table.sh
@@ -0,0 +1,7 @@
+aws dynamodb create-table \
+    --profile fake \
+    --endpoint-url http://localhost:8000 \
+    --table-name MusicCollection \
+    --attribute-definitions AttributeName=Artist,AttributeType=S AttributeName=SongTitle,AttributeType=S \
+    --key-schema AttributeName=Artist,KeyType=HASH AttributeName=SongTitle,KeyType=RANGE \
+    --billing-mode PAY_PER_REQUEST
